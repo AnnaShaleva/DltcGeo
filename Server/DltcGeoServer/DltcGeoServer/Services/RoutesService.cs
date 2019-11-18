e8b68cb027e1c98df174c5acc60c88d4a88bc194
Anna Shaleva <shaleva.ann@gmail.com>
2019-11-18 14:42:09 +0300

    Added server core

---
+++ Server/DltcGeoServer/DltcGeoServer/Services/RoutesService.cs
@@ -0,0 +1,100 @@
+﻿using DltcGeoServer.Models;
+using Itinero;
+using Itinero.Exceptions;
+using Itinero.IO.Osm;
+using Itinero.Profiles;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DltcGeoServer.Services
+{
+    public class RoutesService
+    {
+        private readonly RouterDb _routerDb;
+        private readonly Router _router;
+
+        public RoutesService()
+        {
+            _routerDb = new RouterDb();
+
+            var stream = new FileStream("/app/LO.pbf", FileMode.Open);
+            {
+                try
+                {
+                    _routerDb.LoadOsmData(stream, new[]
+                    {
+                        Itinero.Osm.Vehicles.Vehicle.Car,
+                        Itinero.Osm.Vehicles.Vehicle.Pedestrian
+                    });
+                    _router = new Router(_routerDb);
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public IEnumerable<Point> GetPath(Point start, Point end, List<Profile> profiles)
+        {
+            Route resultRoute = null;
+
+            foreach (var profile in profiles)
+            {
+                try
+                {
+                    var route = _router.Calculate(profile, (float)start.Latitude, (float)start.Longitude, (float)end.Latitude, (float)end.Longitude);
+                    if (resultRoute == null || route.TotalDistance < resultRoute.TotalDistance)
+                        resultRoute = route;
+                }
+                catch (ResolveFailedException e)
+                {
+                    Debug.WriteLine($"Выкинута точка {end.Latitude} {end.Longitude}");
+                }
+                catch (RouteNotFoundException e)
+                {
+                    Debug.WriteLine($"Путь не найден между точками: ({start.Latitude} {start.Longitude}) и ({end.Latitude} {end.Longitude})");
+                }
+            }
+
+            return resultRoute
+                ?.Shape
+                ?.Select(p => new Point
+                {
+                    Latitude = p.Latitude,
+                    Longitude = p.Longitude
+                });
+        }
+
+        public IEnumerable<Point> GetPathForGroup(List<Point> points, List<Profile> profiles)
+        {
+            Route resultRoute = null;
+
+            foreach (var profile in profiles)
+            {
+                try
+                {
+                    var route = _router.Calculate(profile, points.Select(p => new Itinero.LocalGeo.Coordinate((float)p.Latitude, (float)p.Longitude)).ToArray());
+                    if (resultRoute == null || resultRoute.TotalDistance < route.TotalDistance)
+                        resultRoute = route;
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine($"Невозможность");
+                }
+            }
+
+            return resultRoute
+                ?.Shape
+                ?.Select(p => new Point
+                {
+                    Latitude = p.Latitude,
+                    Longitude = p.Longitude
+                });
+        }
+    }
+}
--- Server/DltcGeoServer/DltcGeoServer/Startup.cs
+++ Server/DltcGeoServer/DltcGeoServer/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DltcGeoServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace DltcGeoServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<RoutesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
